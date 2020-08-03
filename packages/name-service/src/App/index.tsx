f09aa6d8221cde6d62cf82f7e23d24727d7b3ea9
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-03 20:28:05 +0200

    Use better colors and showcase color formats

--- packages/name-service/src/App/index.tsx
+++ packages/name-service/src/App/index.tsx
@@ -7,9 +7,10 @@ const { Title, Paragraph } = Typography;
 const App = (): JSX.Element => (
   <div className="App">
     <Typography>
-      <Title>This text should be red instead of black</Title>
+      <Title>This title should be red instead of black</Title>
       <Paragraph>
-        This text should be blue instead of black and the button should be green instead of blue
+        This paragraph should be blue instead of black. The button below should be orange with black text,
+        instead of blue with white text
       </Paragraph>
     </Typography>
     <Button type="primary">Button</Button>
