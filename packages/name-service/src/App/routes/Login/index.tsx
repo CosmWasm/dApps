ec0c94c3b67b12ce4dc2524087bb99887c1cdc29
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-05 16:51:57 +0200

    Add Login route

---
+++ packages/name-service/src/App/routes/Login/index.tsx
@@ -0,0 +1,35 @@
+import { Button, Typography } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import Center from "../../../theme/layout/Center";
+import Stack from "../../../theme/layout/Stack";
+import { pathHome } from "../../paths";
+import "./Login.less";
+
+const { Title, Text } = Typography;
+
+function Login(): JSX.Element {
+  return (
+    <Center tag="main" className="Login">
+      <Stack className="MainStack">
+        <Typography>
+          <Title>CosmWasm</Title>
+        </Typography>
+        <Stack className="WelcomeStack">
+          <Typography>
+            <Title level={2}>Hello!</Title>
+            <Text className="LightText">Select one of the following options to start:</Text>
+          </Typography>
+          <Link to={pathHome}>
+            <Button type="primary">Browser (Demo)</Button>
+          </Link>
+          <Button disabled type="primary">
+            Keplr (Secure)
+          </Button>
+        </Stack>
+      </Stack>
+    </Center>
+  );
+}
+
+export default Login;
