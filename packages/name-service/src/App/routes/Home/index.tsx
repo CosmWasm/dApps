c6629470b63348a211c32f4ff3026175c130349f
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-05 16:52:08 +0200

    Add Home route

---
+++ packages/name-service/src/App/routes/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Button, Typography } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import Center from "../../../theme/layout/Center";
+import Stack from "../../../theme/layout/Stack";
+import YourAccount from "../../components/YourAccount";
+import { pathLuxury } from "../../paths";
+import "./Home.less";
+
+const { Title } = Typography;
+
+function Home(): JSX.Element {
+  return (
+    <Center tag="main" className="Home">
+      <Stack className="MainStack">
+        <Typography>
+          <Title>Name Service</Title>
+        </Typography>
+        <Stack tag="nav">
+          <Button disabled type="primary">
+            Free
+          </Button>
+          <Link to={pathLuxury}>
+            <Button type="primary">Luxury</Button>
+          </Link>
+          <Button disabled type="primary">
+            Tokens
+          </Button>
+        </Stack>
+        <YourAccount tag="footer" />
+      </Stack>
+    </Center>
+  );
+}
+
+export default Home;
