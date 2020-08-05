a420f49887d884a19358f9383f115ec99915f008
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-05 16:52:42 +0200

    Add YourAccount component

---
+++ packages/name-service/src/App/components/YourAccount/index.tsx
@@ -0,0 +1,31 @@
+import { Button, Divider, Typography } from "antd";
+import React from "react";
+import Center from "../../../theme/layout/Center";
+import Stack, { StackProps } from "../../../theme/layout/Stack";
+import "./YourAccount.less";
+
+const { Title, Text } = Typography;
+
+function YourAccount(props: StackProps): JSX.Element {
+  return (
+    <Center className="YourAccount" {...props}>
+      <Stack>
+        <header>
+          <Typography>
+            <Title level={3}>Your Account</Title>
+          </Typography>
+          <Divider />
+        </header>
+        <Typography>
+          <Text>cosmos1jllyqyelctsqhdq6jkdnyf7900ollfnfkfop0090</Text>
+          <Text>(7.995 COSM)</Text>
+        </Typography>
+        <Button disabled type="primary">
+          Copy Account Address
+        </Button>
+      </Stack>
+    </Center>
+  );
+}
+
+export default YourAccount;
