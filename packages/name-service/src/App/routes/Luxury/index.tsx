e36558343fd8ab9053e1c34ac9b909206a27b117
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-05 16:53:09 +0200

    Add Luxury route

---
+++ packages/name-service/src/App/routes/Luxury/index.tsx
@@ -0,0 +1,42 @@
+import { Input, Typography } from "antd";
+import React, { useState } from "react";
+import Center from "../../../theme/layout/Center";
+import Stack from "../../../theme/layout/Stack";
+import YourAccount from "../../components/YourAccount";
+import "./Luxury.less";
+import SearchResult from "./SearchResult";
+
+const { Title, Text } = Typography;
+const { Search } = Input;
+
+function Luxury(): JSX.Element {
+  const [searchedName, setSearchedName] = useState("");
+
+  return (
+    <Center tag="main" className="Luxury">
+      <Stack className="MainStack">
+        <Stack className="SearchAndResultStack">
+          <Stack className="SearchStack">
+            <Typography>
+              <Title>Luxury</Title>
+            </Typography>
+            <Typography>
+              <Text>(cosmos1ym5m5dw7ptt7jkf8f0k3k3o30889ju</Text>
+            </Typography>
+            <Search
+              placeholder="Name"
+              enterButton
+              onSearch={(name) => {
+                setSearchedName(name);
+              }}
+            />
+          </Stack>
+          <SearchResult name={searchedName} />
+        </Stack>
+        <YourAccount tag="footer" />
+      </Stack>
+    </Center>
+  );
+}
+
+export default Luxury;
