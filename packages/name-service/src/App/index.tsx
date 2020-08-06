1cbdee2c6276be4b8637c4f88efe8a874f94b702
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-06 15:52:01 +0200

    Add OperationResult route in Router

--- packages/name-service/src/App/index.tsx
+++ packages/name-service/src/App/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Loading from "./components/Loading";
-import { pathHome, pathLogin, pathLuxury, pathTestLoading, pathTransfer } from "./paths";
+import { pathHome, pathLogin, pathLuxury, pathOperationResult, pathTestLoading, pathTransfer } from "./paths";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import Luxury from "./routes/Luxury";
+import OperationResult from "./routes/OperationResult";
 import Transfer from "./routes/Transfer";
 
 function App(): JSX.Element {
@@ -16,6 +17,7 @@ function App(): JSX.Element {
         <Route exact path={pathHome} component={Home} />
         <Route exact path={pathLuxury} component={Luxury} />
         <Route exact path={pathTransfer} component={Transfer} />
+        <Route exact path={pathOperationResult} component={OperationResult} />
         <Route exact path={pathTestLoading} component={Loading} />
       </Switch>
     </Router>
--- packages/name-service/src/App/paths.ts
+++ packages/name-service/src/App/paths.ts
@@ -2,5 +2,6 @@ export const pathLogin = "/login";
 export const pathHome = "/home";
 export const pathLuxury = "/luxury";
 export const pathTransfer = "/transfer";
+export const pathOperationResult = "/result";
 
 export const pathTestLoading = "/loading";
