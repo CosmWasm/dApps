4cd2641f3d3508a536934bb578cd6b5d6438c7ca
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-07 16:34:56 +0200

    Make BackButton a svg

--- packages/name-service/src/App/components/BackButton/index.tsx
+++ packages/name-service/src/App/components/BackButton/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import backArrow from "./assets/backArrow.svg";
 import "./BackButton.less";
 
 function BackButton(): JSX.Element {
   const history = useHistory();
 
-  return (
-    <div className="BackButton" onClick={history.goBack}>
-      {"<-"}
-    </div>
-  );
+  return <img src={backArrow} alt="Back arrow" className="BackButton" onClick={history.goBack} />;
 }
 
 export default BackButton;
