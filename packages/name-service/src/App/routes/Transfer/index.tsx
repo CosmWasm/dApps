02f002abd48e92699054e1e331c351569a7d3a37
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-10 16:37:59 +0200

    Add transfer logic

--- packages/name-service/src/App/routes/Transfer/index.tsx
+++ packages/name-service/src/App/routes/Transfer/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Input, Typography } from "antd";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import Center from "../../../theme/layout/Center";
 import Stack from "../../../theme/layout/Stack";
@@ -7,14 +7,51 @@ import BackButton from "../../components/BackButton";
 import YourAccount from "../../components/YourAccount";
 import { pathOperationResult } from "../../paths";
 import "./Transfer.less";
+import { Prices } from "../Luxury/SearchResult";
+import { useSdk, useAccount, useError } from "../../../service";
 
 const { Title, Text } = Typography;
 
+interface TransferState {
+  readonly name: string;
+  readonly contractAddress: string;
+}
+
 function Transfer(): JSX.Element {
-  const name = useLocation().state as string;
+  const { name, contractAddress } = useLocation().state as TransferState;
+  const { getClient } = useSdk();
+  const accountProvider = useAccount();
+  const { setError } = useError();
   const history = useHistory();
+  const [newOwnerAddress, setNewOwnerAddress] = useState("");
+  const [prices, setPrices] = useState<Prices>({});
+
+  React.useEffect(() => {
+    getClient()
+      .queryContractSmart(contractAddress, { config: {} })
+      .then((response) => {
+        setPrices({
+          transfer: response.transfer_price,
+        });
+      })
+      .catch(setError);
+  }, [setError, contractAddress, getClient]);
 
   function tryTransfer() {
+    const transferPrice = prices.transfer;
+    const payment = transferPrice ? [transferPrice] : undefined;
+    getClient()
+      .execute(
+        contractAddress,
+        { transfer: { name: name, to: newOwnerAddress } },
+        "Transferring my name",
+        payment,
+      )
+      .then(() => {
+        accountProvider.refreshAccount();
+      })
+      .catch(setError);
+
     history.push({
       pathname: pathOperationResult,
       state: { success: false, message: "Could not transfer name" },
@@ -32,7 +69,7 @@ function Transfer(): JSX.Element {
             <Text>{name}</Text>
           </Typography>
           <Text>to</Text>
-          <Input placeholder="Enter address" />
+          <Input placeholder="Enter address" onChange={(event) => setNewOwnerAddress(event.target.value)} />
           <Button type="primary" onClick={tryTransfer}>
             Transfer 1 COSM
           </Button>
