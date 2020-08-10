037df4ea2d7756095c3b8ed8c2cef8b1be11b585
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-10 13:45:31 +0200

    Make Home view load contracts from chain

--- packages/name-service/src/App/routes/Home/index.tsx
+++ packages/name-service/src/App/routes/Home/index.tsx
@@ -1,29 +1,49 @@
+import { Contract } from "@cosmjs/cosmwasm";
 import { Button, Typography } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { config } from "../../../config";
+import { useAccount, useError, useSdk } from "../../../service";
 import Center from "../../../theme/layout/Center";
 import Stack from "../../../theme/layout/Stack";
+import Loading from "../../components/Loading";
 import YourAccount from "../../components/YourAccount";
-import { pathLuxury } from "../../paths";
 import "./Home.less";
 
 const { Title } = Typography;
 
 function Home(): JSX.Element {
-  return (
+  const sdk = useSdk();
+  const accountProvider = useAccount();
+  const { setError } = useError();
+  const [contracts, setContracts] = useState<readonly Contract[]>([]);
+
+  useEffect(() => {
+    sdk.init();
+    accountProvider.refreshAccount();
+  }, [sdk, accountProvider]);
+
+  useEffect(() => {
+    !sdk.loading &&
+      sdk
+        .getClient()
+        .getContracts(config.codeId)
+        .then((contracts) => setContracts(contracts))
+        .catch(setError);
+  }, [sdk, setError]);
+
+  return sdk.loading ? (
+    <Loading loadingText="Initializing app..." />
+  ) : (
     <Center tag="main" className="Home">
       <Stack>
         <Title>Name Service</Title>
         <Stack tag="nav">
-          <Button disabled type="primary">
-            Free
-          </Button>
-          <Link to={pathLuxury}>
-            <Button type="primary">Luxury</Button>
-          </Link>
-          <Button disabled type="primary">
-            Tokens
-          </Button>
+          {contracts.map(({ label, address }) => (
+            <Link key={address} to={`/${label.toLowerCase()}/${address}`}>
+              <Button type="primary">{label}</Button>
+            </Link>
+          ))}
         </Stack>
         <YourAccount tag="footer" />
       </Stack>
