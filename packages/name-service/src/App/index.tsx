daf37a55f79500daa10f33b0068a889f385b3fb4
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-10 13:43:12 +0200

    Adds provider and param for Luxury view

--- packages/name-service/src/App/index.tsx
+++ packages/name-service/src/App/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Loading from "./components/Loading";
-import { pathHome, pathLogin, pathLuxury, pathOperationResult, pathTestLoading, pathTransfer } from "./paths";
+import { config } from "../config";
+import { AccountProvider, BurnerWalletProvider } from "../service";
+import { pathHome, pathLogin, pathLuxury, pathOperationResult, pathTransfer } from "./paths";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import Luxury from "./routes/Luxury";
@@ -10,16 +11,20 @@ import Transfer from "./routes/Transfer";
 
 function App(): JSX.Element {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path={pathLogin} component={Login} />
-        <Route exact path={pathHome} component={Home} />
-        <Route exact path={pathLuxury} component={Luxury} />
-        <Route exact path={pathTransfer} component={Transfer} />
-        <Route exact path={pathOperationResult} component={OperationResult} />
-      </Switch>
-    </Router>
+    <BurnerWalletProvider config={config}>
+      <AccountProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path={pathLogin} component={Login} />
+            <Route exact path={pathHome} component={Home} />
+            <Route exact path={`${pathLuxury}/:address`} component={Luxury} />
+            <Route exact path={pathTransfer} component={Transfer} />
+            <Route exact path={pathOperationResult} component={OperationResult} />
+          </Switch>
+        </Router>
+      </AccountProvider>
+    </BurnerWalletProvider>
   );
 }
 
