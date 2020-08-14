f93795ca7e8fba303aee6de5197a13cf6f372ec8
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-14 14:51:18 +0200

    Add coralnet AppConfig

--- packages/name-service/src/config.ts
+++ packages/name-service/src/config.ts
@@ -13,6 +13,7 @@ const local: AppConfig = {
   codeId: 2,
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const demonet: AppConfig = {
   httpUrl: "https://lcd.demo-10.cosmwasm.com",
   faucetUrl: "https://faucet.demo-10.cosmwasm.com/credit",
@@ -20,7 +21,14 @@ const demonet: AppConfig = {
   codeId: 2,
 };
 
+const coralnet: AppConfig = {
+  httpUrl: "https://lcd.coralnet.cosmwasm.com",
+  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
+  addressPrefix: "coral",
+  codeId: 2,
+};
+
 // REACT_APP_LOCAL is set via `yarn start:local`
 const isLocal = process.env.NODE_ENV !== "production" && !!process.env.REACT_APP_LOCAL;
 
-export const config = isLocal ? local : demonet;
+export const config = isLocal ? local : coralnet;
