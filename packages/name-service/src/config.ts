33ddc8b02edefa6d9647f6d9056410dc09e324e3
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-14 17:05:49 +0200

    Add new config fields and use buildFeeTable

--- packages/name-service/src/config.ts
+++ packages/name-service/src/config.ts
@@ -1,7 +1,9 @@
 export interface AppConfig {
   readonly httpUrl: string;
+  readonly feeToken: string;
+  readonly gasPrice: 0.025;
   readonly faucetUrl?: string;
-  readonly ticker?: string;
+  readonly faucetToken?: string;
   readonly addressPrefix: string;
   // codeId is the wasm codeId for the name service contract on the given chain
   readonly codeId: number;
@@ -9,8 +11,10 @@ export interface AppConfig {
 
 const local: AppConfig = {
   httpUrl: "http://localhost:1317",
+  feeToken: "ucosm",
+  gasPrice: 0.025,
   faucetUrl: "http://localhost:8000/credit",
-  ticker: "COSM",
+  faucetToken: "COSM",
   addressPrefix: "cosmos",
   codeId: 2,
 };
@@ -18,16 +22,20 @@ const local: AppConfig = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const demonet: AppConfig = {
   httpUrl: "https://lcd.demo-10.cosmwasm.com",
+  feeToken: "ucosm",
+  gasPrice: 0.025,
   faucetUrl: "https://faucet.demo-10.cosmwasm.com/credit",
-  ticker: "COSM",
+  faucetToken: "COSM",
   addressPrefix: "cosmos",
   codeId: 2,
 };
 
 const coralnet: AppConfig = {
   httpUrl: "https://lcd.coralnet.cosmwasm.com",
+  feeToken: "ushell",
+  gasPrice: 0.025,
   faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
-  ticker: "SHELL",
+  faucetToken: "SHELL",
   addressPrefix: "coral",
   codeId: 2,
 };
