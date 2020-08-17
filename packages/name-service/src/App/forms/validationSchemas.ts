7b8c162ba3231dab4e577723c291a4613fc1fc55
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-17 08:37:12 +0200

    Support any prefix in transfer form

--- packages/name-service/src/App/forms/validationSchemas.ts
+++ packages/name-service/src/App/forms/validationSchemas.ts
@@ -1,9 +1,15 @@
 import * as Yup from "yup";
+import { config } from "../../config";
 
 export const SearchValidationSchema = Yup.object().shape({
   name: Yup.string().min(4, "Name can not be less than 4 characters").required("A Name is required"),
 });
 
+const regexStartsWithPrefix = new RegExp(`^${config.addressPrefix}`);
+
 export const TransferValidationSchema = Yup.object().shape({
-  address: Yup.string().min(45, "Address invalid").required("An address is required"),
+  address: Yup.string()
+    .required("An address is required")
+    .matches(regexStartsWithPrefix, `"${config.addressPrefix}" prefix required`)
+    .length(39 + config.addressPrefix.length, "Address invalid"),
 });
