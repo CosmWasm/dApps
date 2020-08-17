b8d3fbb9a968b30f896707577128ef17214aa879
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-17 20:48:11 +0200

    Updates

---
+++ names/precache-manifest.114a7753e6b5572c96be7198e17adc8e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be9a4b18949ab7d59f3bebeadfd809a",
+    "url": "/dApps/names/index.html"
+  },
+  {
+    "revision": "3036da8f85683f9f503c",
+    "url": "/dApps/names/static/css/2.ab37708d.chunk.css"
+  },
+  {
+    "revision": "74df459422b339c332ce",
+    "url": "/dApps/names/static/css/main.286c3914.chunk.css"
+  },
+  {
+    "revision": "3036da8f85683f9f503c",
+    "url": "/dApps/names/static/js/2.e3c5ff13.chunk.js"
+  },
+  {
+    "revision": "b1f248559ba67264dcc58e82c33d7fd4",
+    "url": "/dApps/names/static/js/2.e3c5ff13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74df459422b339c332ce",
+    "url": "/dApps/names/static/js/main.e7969189.chunk.js"
+  },
+  {
+    "revision": "fbe1ad4855e18deb2bcf",
+    "url": "/dApps/names/static/js/runtime-main.2fe88d24.js"
+  },
+  {
+    "revision": "f6c4b6163a3eed620e208f02be87b357",
+    "url": "/dApps/names/static/media/Iceland-Regular.f6c4b616.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/dApps/names/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/dApps/names/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/dApps/names/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9849fc2ec5bc8824532c0d58f59f2c04",
+    "url": "/dApps/names/static/media/backArrow.9849fc2e.svg"
+  },
+  {
+    "revision": "15dccd19b2d06d88a657e306a4349307",
+    "url": "/dApps/names/static/media/cosmWasmLogo.15dccd19.svg"
+  },
+  {
+    "revision": "71513020fdd4db4321b81c43daf03518",
+    "url": "/dApps/names/static/media/failIcon.71513020.svg"
+  },
+  {
+    "revision": "2e305b6693ad95cd56039373d2051bc2",
+    "url": "/dApps/names/static/media/successIcon.2e305b66.svg"
+  }
+]);
--- names/service-worker.js
+++ names/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/names/precache-manifest.0364aa7c66e5343ee32accd6887c5c15.js"
+  "/dApps/names/precache-manifest.114a7753e6b5572c96be7198e17adc8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/names/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dApps/names/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
