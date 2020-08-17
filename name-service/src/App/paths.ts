2a781c3c217da3de68add7b080a6bc5d929f5d13
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-17 12:55:34 +0200

    Updates

--- name-service/src/App/paths.ts
+++
@@ -1,5 +0,0 @@
-export const pathLogin = "/login";
-export const pathHome = "/home";
-export const pathContract = "/contract";
-export const pathTransfer = "/transfer";
-export const pathOperationResult = "/result";
