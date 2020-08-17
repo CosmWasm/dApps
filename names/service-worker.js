8e288d104b770e8c3e7d50598b215ea1efc9d04e
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-17 16:58:20 +0200

    Updates

--- names/service-worker.js
+++ names/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/names/precache-manifest.a9efbec99e373e1e4de160b6d8ca74a1.js"
+  "/dApps/names/precache-manifest.83b1f737808f4b093d9e95a9406332ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/names/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dApps/names/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
