50f515659a8b1497214617f97b7ebae463375245
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-19 12:35:56 +0200

    Add new app to deployment

--- gh-pages-config/.predeploy.sh
+++ gh-pages-config/.predeploy.sh
@@ -5,3 +5,4 @@ yarn build
 
 # Copy the packages we want to publish with desired url name
 cp -R packages/name-service/build gh-pages-config/names
+cp -R packages/wallet/build gh-pages-config/wallet
