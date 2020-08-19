348b483c0d4888bb9e5f1cbd55d915c9d01b02dc
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-19 12:35:35 +0200

    Add app with dummy login

---
+++ packages/wallet/src/App/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { config } from "../config";
+import { AccountProvider, BurnerWalletProvider, ErrorProvider } from "../service";
+import Login from "./routes/Login";
+
+function App(): JSX.Element {
+  return (
+    <ErrorProvider>
+      <BurnerWalletProvider config={config}>
+        <AccountProvider>
+          <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+              <Route exact path="/" component={Login} />
+            </Switch>
+          </Router>
+        </AccountProvider>
+      </BurnerWalletProvider>
+    </ErrorProvider>
+  );
+}
+
+export default App;
