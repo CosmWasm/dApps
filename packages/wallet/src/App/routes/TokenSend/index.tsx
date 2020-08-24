5bdf6a8b5879598fa66de4e5033d5779349fc5cb
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-24 12:52:01 +0200

    Add TokenSend component

---
+++ packages/wallet/src/App/routes/TokenSend/index.tsx
@@ -0,0 +1,57 @@
+import { Button, Typography, Input } from "antd";
+import React from "react";
+import { useHistory, useParams } from "react-router-dom";
+import Center from "../../../theme/layout/Center";
+import Stack from "../../../theme/layout/Stack";
+import BackButton from "../../components/BackButton";
+import YourAccount from "../../components/YourAccount";
+import { pathTokens } from "../../paths";
+import { TokenDetailState } from "../TokenDetail";
+import "./TokenSend.less";
+
+const { Title, Text } = Typography;
+
+interface TokenSendParams {
+  readonly tokenName: string;
+}
+
+export interface TokenSendState {
+  readonly tokenAmount: number;
+}
+
+function TokenSend(): JSX.Element {
+  const history = useHistory<TokenSendState>();
+
+  const { tokenName }: TokenSendParams = useParams();
+  const { tokenAmount }: TokenSendState = history.location.state;
+
+  const tokenDetailState: TokenDetailState = { tokenAmount };
+
+  return (
+    <Center tag="main" className="TokenSend">
+      <Stack className="MainStack">
+        <BackButton path={`${pathTokens}/${tokenName}`} state={tokenDetailState} />
+        <Stack className="AccountStack">
+          <Title>Your Account</Title>
+          <YourAccount showTitle={false} />
+        </Stack>
+        <Stack className="SendStack">
+          <Stack className="FormStack">
+            <div>
+              <Text>Send</Text>
+              <Input placeholder="Enter amount" />
+              <Text>{tokenName}</Text>
+            </div>
+            <div>
+              <Text>to</Text>
+              <Input placeholder="Enter address" />
+            </div>
+          </Stack>
+          <Button type="primary">Send</Button>
+        </Stack>
+      </Stack>
+    </Center>
+  );
+}
+
+export default TokenSend;
