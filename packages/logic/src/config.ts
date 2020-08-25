4e85c26ff8883f57a7115c5f439bba3dd6ff82db
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-25 09:44:20 +0200

    Make all AppConfig fields optional.

--- packages/logic/src/config.ts
+++ packages/logic/src/config.ts
@@ -1,10 +1,10 @@
 export interface AppConfig {
-  readonly httpUrl: string;
-  readonly feeToken: string;
-  readonly gasPrice: number;
+  readonly httpUrl?: string;
+  readonly feeToken?: string;
+  readonly gasPrice?: number;
   readonly faucetUrl?: string;
   readonly faucetToken?: string;
-  readonly addressPrefix: string;
+  readonly addressPrefix?: string;
   // codeId is the wasm codeId for the name service contract on the given chain
-  readonly codeId: number;
+  readonly codeId?: number;
 }
