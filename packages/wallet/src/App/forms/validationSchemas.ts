f14635669851fd232f3954b81f189b309c8b9ebe
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-26 13:29:15 +0200

    Extract common address shape from schemas

--- packages/wallet/src/App/forms/validationSchemas.ts
+++ packages/wallet/src/App/forms/validationSchemas.ts
@@ -3,15 +3,13 @@ import { config } from "../../config";
 
 const regexStartsWithPrefix = new RegExp(`^${config.addressPrefix}`);
 
-export const searchValidationSchema = Yup.object().shape({
-  address: Yup.string()
-    .matches(regexStartsWithPrefix, `"${config.addressPrefix}" prefix required`)
-    .length(39 + config.addressPrefix.length, "Address invalid"),
-});
-
-export const sendAddressValidationSchema = Yup.object().shape({
+const addressShape = {
   address: Yup.string()
     .required("An address is required")
     .matches(regexStartsWithPrefix, `"${config.addressPrefix}" prefix required`)
     .length(39 + config.addressPrefix.length, "Address invalid"),
-});
+};
+
+export const searchValidationSchema = Yup.object().shape(addressShape);
+
+export const sendAddressValidationSchema = Yup.object().shape(addressShape);
