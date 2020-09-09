f130791725ac6e9448475089800399d6ea123581
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-09 21:24:26 +0200

    Add TokensAdd route

---
+++ packages/cw20-wallet/src/App/routes/TokensAdd/index.tsx
@@ -0,0 +1,168 @@
+import { BackButton, OperationResultState, PageLayout } from "@cosmicdapp/design";
+import { getErrorFromStackTrace, useError, useSdk } from "@cosmicdapp/logic";
+import { Contract } from "@cosmjs/cosmwasm";
+import { Button, Typography } from "antd";
+import { Formik } from "formik";
+import { Form, FormItem, Input, Transfer } from "formik-antd";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import backArrowIcon from "../../assets/backArrow.svg";
+import { pathOperationResult, pathTokens, pathTokensAdd } from "../../paths";
+import { useContracts } from "../../service/contracts";
+import { CW20 } from "../../service/cw20";
+import { FormStack, MainStack } from "./style";
+
+const { Title } = Typography;
+
+function TokensAdd(): JSX.Element {
+  const { getClient } = useSdk();
+  const { setError } = useError();
+  const { addContract } = useContracts();
+  const history = useHistory();
+
+  const [codeId, setCodeId] = useState<number>();
+
+  const [contracts, setContracts] = useState<readonly Contract[]>([]);
+  const [selectedContractAddresses, setSelectedContractAddresses] = useState<string[]>([]);
+
+  function submitInputContract({ contract }) {
+    const codeId = Number.isSafeInteger(parseInt(contract, 10)) && Number(contract);
+
+    if (codeId) {
+      getClient()
+        .getContracts(codeId)
+        .then((contracts) => {
+          setContracts(contracts);
+          setCodeId(codeId);
+        })
+        .catch(setError);
+    } else {
+      try {
+        const contractAddress = String(contract);
+        const newCw20contract = CW20(getClient()).use(contractAddress);
+        addContract(newCw20contract);
+
+        history.push({
+          pathname: pathOperationResult,
+          state: {
+            success: true,
+            message: `"${contractAddress}" was successfully added :)`,
+            customButtonText: "Tokens",
+          } as OperationResultState,
+        });
+      } catch (stackTrace) {
+        history.push({
+          pathname: pathOperationResult,
+          state: {
+            success: false,
+            message: "Oh no... Something went wrong, please try again",
+            error: getErrorFromStackTrace(stackTrace),
+            customButtonActionPath: pathTokensAdd,
+          } as OperationResultState,
+        });
+      }
+    }
+  }
+
+  function handleChangeSelected(selectedAddresses: string[]) {
+    setSelectedContractAddresses(selectedAddresses);
+  }
+
+  function submitSelectContracts() {
+    try {
+      selectedContractAddresses.forEach((address) => {
+        const newCw20Contract = CW20(getClient()).use(address);
+        addContract(newCw20Contract);
+      });
+
+      history.push({
+        pathname: pathOperationResult,
+        state: {
+          success: true,
+          message: `"Your CodeID: ${codeId}" were successfully added :)`,
+          customButtonText: "Tokens",
+        } as OperationResultState,
+      });
+    } catch (stackTrace) {
+      history.push({
+        pathname: pathOperationResult,
+        state: {
+          success: false,
+          message: "Oh no... Something went wrong, please try again",
+          error: getErrorFromStackTrace(stackTrace),
+          customButtonActionPath: pathTokensAdd,
+        } as OperationResultState,
+      });
+    }
+  }
+
+  const showInputContract = !codeId;
+  const showCodeiIdForm = !showInputContract;
+
+  return (
+    <PageLayout>
+      <BackButton icon={backArrowIcon} path={pathTokens} />
+      <MainStack>
+        <Title>Add Other</Title>
+        {showInputContract && (
+          <Formik
+            initialValues={{ contract: "" }}
+            onSubmit={submitInputContract}
+            //TODO: fix validation schema for address + codeID number and enable it
+            /* validationSchema={contractValidationSchema} */
+          >
+            {(formikProps) => (
+              <Form>
+                <FormStack>
+                  <FormItem name="contract">
+                    <Input name="contract" placeholder="Enter a contract address or codeID" />
+                  </FormItem>
+                  <Button
+                    type="primary"
+                    onClick={formikProps.submitForm}
+                    disabled={!(formikProps.isValid && formikProps.dirty)}
+                  >
+                    Continue
+                  </Button>
+                </FormStack>
+              </Form>
+            )}
+          </Formik>
+        )}
+        {showCodeiIdForm && (
+          <Formik initialValues={{}} onSubmit={submitSelectContracts}>
+            {(formikProps) => (
+              <Form>
+                <FormStack>
+                  <FormItem name="contracts">
+                    <Transfer
+                      name="contracts"
+                      showSearch
+                      pagination={{ pageSize: 5 }}
+                      dataSource={contracts.map((contract) => {
+                        return { key: contract.address, title: contract.label };
+                      })}
+                      onSelectChange={handleChangeSelected}
+                      listStyle={{ listStyle: "none" }}
+                      render={(item) => item.title}
+                    />
+                  </FormItem>
+                  <Button
+                    type="primary"
+                    onClick={formikProps.submitForm}
+                    //TODO: fix disabled behavior
+                    /* disabled={!(formikProps.isValid && formikProps.dirty)} */
+                  >
+                    Continue
+                  </Button>
+                </FormStack>
+              </Form>
+            )}
+          </Formik>
+        )}
+      </MainStack>
+    </PageLayout>
+  );
+}
+
+export default TokensAdd;
---
+++ packages/cw20-wallet/src/App/routes/TokensAdd/style.ts
@@ -0,0 +1,44 @@
+import { Stack } from "@cosmicdapp/design";
+import styled from "styled-components";
+
+const MainStack = styled(Stack)`
+  & > * {
+    --gap: var(--s4);
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  .ant-transfer {
+    width: 100%;
+
+    & > *:first-child {
+      width: 100%;
+    }
+
+    & > *:not(:first-child) {
+      display: none;
+    }
+
+    .ant-transfer-list-content-item-text {
+      text-align: left;
+    }
+  }
+`;
+
+const FormStack = styled(Stack)`
+  & > * {
+    --gap: var(--s4);
+  }
+
+  .ant-form-item {
+    margin-bottom: 0;
+  }
+
+  button {
+    margin-top: var(--s2);
+  }
+`;
+
+export { MainStack, FormStack };
