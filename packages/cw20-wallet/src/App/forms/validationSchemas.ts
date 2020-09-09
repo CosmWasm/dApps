6e8a9a2fe74baaac8bb8b8826111e5e31e1d7c57
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-09 21:22:16 +0200

    Add common assets and form utils

---
+++ packages/cw20-wallet/src/App/forms/validationSchemas.ts
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+import { config } from "../../config";
+
+const regexStartsWithPrefix = new RegExp(`^${config.addressPrefix}`);
+
+const addressField = Yup.string()
+  .matches(regexStartsWithPrefix, `"${config.addressPrefix}" prefix required`)
+  .length(39 + config.addressPrefix.length, "Address invalid");
+
+const requiredAddressField = addressField.required("An address is required");
+
+export const contractValidationSchema = Yup.object().shape({
+  contract: Yup.lazy((value) => {
+    switch (typeof value) {
+      case "number":
+        return Yup.number().positive();
+      case "string":
+        return requiredAddressField;
+      default:
+        return Yup.mixed().required("A contract address or codeID is required");
+    }
+  }),
+});
+
+export const searchAddressValidationSchema = Yup.object().shape({ address: addressField });
