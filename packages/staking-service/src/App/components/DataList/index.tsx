99f28999dae2a8ed25482d9f173c9cd0f426431a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-24 08:38:06 +0200

    Add DataList component

---
+++ packages/staking-service/src/App/components/DataList/index.tsx
@@ -0,0 +1,23 @@
+import { Divider } from "antd";
+import React from "react";
+import { DataRow, KeyText, ListStack, ValueText } from "./style";
+
+export interface DataListProps {
+  readonly [key: string]: string;
+}
+
+export function DataList(dataMap: DataListProps): JSX.Element {
+  return (
+    <ListStack>
+      {Object.entries(dataMap).map(([key, value], index) => (
+        <>
+          {index > 0 && <Divider />}
+          <DataRow>
+            <KeyText>{key}</KeyText>
+            <ValueText>{value}</ValueText>
+          </DataRow>
+        </>
+      ))}
+    </ListStack>
+  );
+}
