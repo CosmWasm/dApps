8520aa47f3e78f52fd4b452fe8ff077de8466568
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-24 08:45:32 +0200

    Add AccountMenu component

---
+++ packages/staking-service/src/App/components/AccountMenu/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { pathHome } from "../../paths";
+import avatarIcon from "./assets/avatar.svg";
+import copyIcon from "./assets/copy.svg";
+import closeMenuButton from "./assets/cross.svg";
+import openMenuButton from "./assets/hamburger.svg";
+import {
+  AddressCopy,
+  AddressCopyBox,
+  AddressText,
+  AvatarName,
+  CloseAccountStack,
+  CloseMenuButton,
+  LinksStack,
+  LinkText,
+  MenuCenter,
+  MenuStack,
+  NameText,
+  OpenMenuButton,
+} from "./style";
+
+export interface AccountMenuProps extends React.HTMLAttributes<HTMLOrSVGElement> {
+  readonly name?: string;
+}
+
+export function AccountMenu({ name, ...props }: AccountMenuProps): JSX.Element {
+  const history = useHistory();
+  function goToHelp() {
+    //TODO: set path to help
+    history.push(pathHome);
+  }
+  function goToLogout() {
+    //TODO: set path to logout
+    history.push(pathHome);
+  }
+
+  const [open, setOpen] = useState(false);
+  const openMenu = () => setOpen(true);
+  const closeMenu = () => setOpen(false);
+
+  const accountName = name ? `${name}'s` : "Your";
+
+  return (
+    <>
+      <OpenMenuButton src={openMenuButton} alt="Open account menu" onClick={openMenu} {...props} />
+      {open && (
+        <MenuCenter {...props}>
+          <MenuStack>
+            <CloseAccountStack>
+              <CloseMenuButton src={closeMenuButton} alt="Close account menu" onClick={closeMenu} />
+              <AvatarName>
+                <img src={avatarIcon} alt="Avatar" />
+                <NameText>{accountName} account</NameText>
+              </AvatarName>
+              <AddressCopyBox>
+                <AddressText>coraladdress234jl2k34ljk234</AddressText>
+                <AddressCopy src={copyIcon} alt="Copy address icon" />
+              </AddressCopyBox>
+            </CloseAccountStack>
+            <LinksStack>
+              <div onClick={goToHelp}>
+                <LinkText>Help</LinkText>
+              </div>
+              <div onClick={goToLogout}>
+                <LinkText>Logout</LinkText>
+              </div>
+            </LinksStack>
+          </MenuStack>
+        </MenuCenter>
+      )}
+    </>
+  );
+}
