846f857041a7da572c48ec7f83990a31dc84ed36
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-24 08:37:49 +0200

    Add routes and common assets

---
+++ packages/staking-service/src/App/paths.ts
@@ -0,0 +1,8 @@
+export const pathLogin = "/login";
+export const pathHome = "/home";
+export const pathDetails = "/details";
+export const pathWallet = "/wallet";
+export const pathPurchase = "/purchase";
+export const pathWithdraw = "/withdraw";
+export const pathClaims = "/claims";
+export const pathOperationResult = "/result";
