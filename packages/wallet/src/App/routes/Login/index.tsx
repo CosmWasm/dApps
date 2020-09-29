4aacfe2f0da58af69816f8d8319c85fa1f14d04b
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-09-29 11:28:50 +0200

    Add addressPrefix for ledger support to both wallets

--- packages/wallet/src/App/routes/Login/index.tsx
+++ packages/wallet/src/App/routes/Login/index.tsx
@@ -1,8 +1,16 @@
 import { Login as LoginDesign } from "@cosmicdapp/design";
 import React from "react";
+import { config } from "../../../config";
 import { pathTokens } from "../../paths";
 import cosmWasmLogo from "./assets/cosmWasmLogo.svg";
 
 export function Login(): JSX.Element {
-  return <LoginDesign pathAfterLogin={pathTokens} appName="Wallet" appLogo={cosmWasmLogo} />;
+  return (
+    <LoginDesign
+      pathAfterLogin={pathTokens}
+      appName="Wallet"
+      appLogo={cosmWasmLogo}
+      addressPrefix={config.addressPrefix}
+    />
+  );
 }
