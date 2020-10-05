794b5e8d1d7a947f29ac39793c628d6d4e2e7593
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-05 20:00:58 +0200

    Add Wallet route

--- packages/staking-service/src/App/routes/Wallet/index.tsx
+++ packages/staking-service/src/App/routes/Wallet/index.tsx
@@ -1,33 +1,67 @@
 import { PageLayout } from "@cosmicdapp/design";
+import { nativeCoinToDisplay, useAccount, useSdk } from "@cosmicdapp/logic";
 import { Button, Typography } from "antd";
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { config } from "../../../config";
 import { DataList } from "../../components/DataList";
 import { HeaderBackMenu } from "../../components/HeaderBackMenu";
-import { pathClaims, pathDetails, pathPurchase, pathWithdraw } from "../../paths";
+import { pathClaims, pathDetail, pathPurchase, pathValidator, pathWithdraw } from "../../paths";
+import { formatShares, StakingDelegation, useStakingValidator } from "../../utils/staking";
 import { ButtonStack, MainStack, NavCenter, TitleNavStack } from "./style";
 
 const { Title } = Typography;
 
-const dataMap = { "Atom/Tokens": "1.175", Balance: "0,000", Claims: "0" };
+const emptyDelegation: StakingDelegation = {
+  delegator_address: "",
+  validator_address: "",
+  shares: "0",
+  balance: { amount: "0", denom: "" },
+};
+
+function getDelegationDataMap(delegation: StakingDelegation) {
+  if (!delegation) return;
+
+  const shares = formatShares(delegation.shares);
+  const balance = nativeCoinToDisplay(delegation.balance, config.coinMap).amount;
+
+  return { Shares: shares, Balance: balance };
+}
+
+interface WalletParams {
+  readonly validatorAddress: string;
+}
 
 export function Wallet(): JSX.Element {
   const history = useHistory();
+  const { getStakingClient } = useSdk();
+  const { account } = useAccount();
+  const { validatorAddress } = useParams<WalletParams>();
+  const validator = useStakingValidator(validatorAddress);
 
-  function goToDetails() {
-    history.push(pathDetails);
+  const [delegation, setDelegation] = useState<StakingDelegation>(emptyDelegation);
+
+  useEffect(() => {
+    getStakingClient()
+      .staking.delegation(account.address, validatorAddress)
+      .then(({ result: delegation }) => setDelegation(delegation))
+      .catch(console.error);
+  }, [getStakingClient, account.address, validatorAddress]);
+
+  function goToValidatorDetail() {
+    history.push(`${pathValidator}/${validatorAddress}${pathDetail}`);
   }
 
   function goToPurchase() {
-    history.push(pathPurchase);
+    history.push(`${pathPurchase}/${validatorAddress}`);
   }
 
   function goToWithdraw() {
-    history.push(pathWithdraw);
+    history.push(`${pathWithdraw}/${validatorAddress}`);
   }
 
   function goToClaims() {
-    history.push(pathClaims);
+    history.push(`${pathClaims}/${validatorAddress}`);
   }
 
   return (
@@ -35,14 +69,14 @@ export function Wallet(): JSX.Element {
       <MainStack>
         <HeaderBackMenu />
         <TitleNavStack>
-          <Title>Iris Net</Title>
+          <Title>{validator?.description.moniker ?? ""}</Title>
           <NavCenter>
-            <Button type="primary" onClick={goToDetails}>
+            <Button type="primary" onClick={goToValidatorDetail}>
               View Details
             </Button>
           </NavCenter>
         </TitleNavStack>
-        <DataList {...dataMap} />
+        <DataList {...getDelegationDataMap(delegation)} />
         <ButtonStack>
           <Button type="primary" onClick={goToPurchase}>
             Buy
