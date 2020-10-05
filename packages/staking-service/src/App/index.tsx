ceb70038e03f29150e10742eed671e2610123823
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-05 20:01:27 +0200

    Add new routes

--- packages/staking-service/src/App/index.tsx
+++ packages/staking-service/src/App/index.tsx
@@ -5,18 +5,20 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { config } from "../config";
 import {
   pathClaims,
-  pathDetails,
-  pathHome,
+  pathDetail,
   pathLogin,
   pathPurchase,
+  pathValidator,
+  pathValidators,
   pathWallet,
   pathWithdraw,
 } from "./paths";
 import { Claims } from "./routes/Claims";
-import { Details } from "./routes/Details";
-import { Home } from "./routes/Home";
 import { Login } from "./routes/Login";
 import { Purchase } from "./routes/Purchase";
+import { ValidatorDetail } from "./routes/ValidatorDetail";
+import { ValidatorHome } from "./routes/ValidatorHome";
+import { Validators } from "./routes/Validators";
 import { Wallet } from "./routes/Wallet";
 import { Withdraw } from "./routes/Withdraw";
 
@@ -31,12 +33,17 @@ export function App(): JSX.Element {
               <Route exact path="/" component={Login} />
               <Route exact path={pathLogin} component={Login} />
               <ProtectedSwitch authPath={pathLogin}>
-                <Route exact path={pathHome} component={Home} />
-                <Route exact path={pathDetails} component={Details} />
-                <Route exact path={pathWallet} component={Wallet} />
-                <Route exact path={pathPurchase} component={Purchase} />
-                <Route exact path={pathWithdraw} component={Withdraw} />
-                <Route exact path={pathClaims} component={Claims} />
+                <Route exact path={pathValidators} component={Validators} />
+                <Route exact path={`${pathValidator}/:validatorAddress`} component={ValidatorHome} />
+                <Route
+                  exact
+                  path={`${pathValidator}/:validatorAddress${pathDetail}`}
+                  component={ValidatorDetail}
+                />
+                <Route exact path={`${pathWallet}/:validatorAddress`} component={Wallet} />
+                <Route exact path={`${pathPurchase}/:validatorAddress`} component={Purchase} />
+                <Route exact path={`${pathWithdraw}/:validatorAddress`} component={Withdraw} />
+                <Route exact path={`${pathClaims}/:validatorAddress`} component={Claims} />
               </ProtectedSwitch>
             </Switch>
           </Router>
