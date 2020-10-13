055da3b8d65938798f29a3231046a6861bde760a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-13 13:59:59 +0200

    Fix OperationResult default path

--- packages/staking-service/src/App/routes/OperationResult/index.tsx
+++ packages/staking-service/src/App/routes/OperationResult/index.tsx
@@ -1,18 +1,18 @@
 import { OperationResult as OperationResultDesign } from "@cosmicdapp/design";
 import { History } from "history";
 import React from "react";
-import { pathValidator } from "../../paths";
+import { pathValidators } from "../../paths";
 import failIcon from "./assets/failIcon.svg";
 import successIcon from "./assets/successIcon.svg";
 
-function goToTokensList(history: History) {
-  history.push(pathValidator);
+function goToValidators(history: History) {
+  history.push(pathValidators);
 }
 
 export function OperationResult(): JSX.Element {
   return (
     <OperationResultDesign
-      defaultButtonAction={goToTokensList}
+      defaultButtonAction={goToValidators}
       successIcon={successIcon}
       failIcon={failIcon}
     />
