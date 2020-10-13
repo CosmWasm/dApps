0ce85ee14479849bc8dbacd0946e4d18b91dd8fc
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-13 14:02:03 +0200

    Add result route and use ContractProvider from logic

--- packages/staking-service/src/App/index.tsx
+++ packages/staking-service/src/App/index.tsx
@@ -1,5 +1,11 @@
 import { GlobalStyle } from "@cosmicdapp/design";
-import { AccountProvider, BurnerWalletProvider, ErrorProvider, ProtectedSwitch } from "@cosmicdapp/logic";
+import {
+  AccountProvider,
+  BurnerWalletProvider,
+  ContractsProvider,
+  ErrorProvider,
+  ProtectedSwitch,
+} from "@cosmicdapp/logic";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { config } from "../config";
@@ -7,6 +13,7 @@ import {
   pathClaims,
   pathDetail,
   pathLogin,
+  pathOperationResult,
   pathPurchase,
   pathValidator,
   pathValidators,
@@ -15,6 +22,7 @@ import {
 } from "./paths";
 import { Claims } from "./routes/Claims";
 import { Login } from "./routes/Login";
+import { OperationResult } from "./routes/OperationResult";
 import { Purchase } from "./routes/Purchase";
 import { ValidatorDetail } from "./routes/ValidatorDetail";
 import { ValidatorHome } from "./routes/ValidatorHome";
@@ -27,26 +35,29 @@ export function App(): JSX.Element {
     <ErrorProvider>
       <BurnerWalletProvider config={config}>
         <AccountProvider>
-          <GlobalStyle />
-          <Router basename={process.env.PUBLIC_URL}>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path={pathLogin} component={Login} />
-              <ProtectedSwitch authPath={pathLogin}>
-                <Route exact path={pathValidators} component={Validators} />
-                <Route exact path={`${pathValidator}/:validatorAddress`} component={ValidatorHome} />
-                <Route
-                  exact
-                  path={`${pathValidator}/:validatorAddress${pathDetail}`}
-                  component={ValidatorDetail}
-                />
-                <Route exact path={`${pathWallet}/:validatorAddress`} component={Wallet} />
-                <Route exact path={`${pathPurchase}/:validatorAddress`} component={Purchase} />
-                <Route exact path={`${pathWithdraw}/:validatorAddress`} component={Withdraw} />
-                <Route exact path={`${pathClaims}/:validatorAddress`} component={Claims} />
-              </ProtectedSwitch>
-            </Switch>
-          </Router>
+          <ContractsProvider>
+            <GlobalStyle />
+            <Router basename={process.env.PUBLIC_URL}>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path={pathLogin} component={Login} />
+                <ProtectedSwitch authPath={pathLogin}>
+                  <Route exact path={pathValidators} component={Validators} />
+                  <Route exact path={`${pathValidator}/:validatorAddress`} component={ValidatorHome} />
+                  <Route
+                    exact
+                    path={`${pathValidator}/:validatorAddress${pathDetail}`}
+                    component={ValidatorDetail}
+                  />
+                  <Route exact path={`${pathWallet}/:validatorAddress`} component={Wallet} />
+                  <Route exact path={`${pathPurchase}/:validatorAddress`} component={Purchase} />
+                  <Route exact path={`${pathWithdraw}/:validatorAddress`} component={Withdraw} />
+                  <Route exact path={`${pathClaims}/:validatorAddress`} component={Claims} />
+                  <Route exact path={pathOperationResult} component={OperationResult} />
+                </ProtectedSwitch>
+              </Switch>
+            </Router>
+          </ContractsProvider>
         </AccountProvider>
       </BurnerWalletProvider>
     </ErrorProvider>
