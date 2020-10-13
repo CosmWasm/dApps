2c5f2fd55c3cf70fdfee3d789b86f7e11d3fed14
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-13 14:02:25 +0200

    Use cw20 in Wallet

--- packages/staking-service/src/App/routes/Wallet/index.tsx
+++ packages/staking-service/src/App/routes/Wallet/index.tsx
@@ -1,31 +1,31 @@
 import { PageLayout } from "@cosmicdapp/design";
-import { nativeCoinToDisplay, useAccount, useSdk } from "@cosmicdapp/logic";
+import { Claims, CW20, Investment, TokenInfo, useAccount, useSdk } from "@cosmicdapp/logic";
+import { Decimal } from "@cosmjs/math";
 import { Button, Typography } from "antd";
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { config } from "../../../config";
 import { DataList } from "../../components/DataList";
 import { HeaderBackMenu } from "../../components/HeaderBackMenu";
 import { pathClaims, pathDetail, pathPurchase, pathValidator, pathWithdraw } from "../../paths";
-import { formatShares, StakingDelegation, useStakingValidator } from "../../utils/staking";
 import { ButtonStack, MainStack, NavCenter, TitleNavStack } from "./style";
 
 const { Title } = Typography;
 
-const emptyDelegation: StakingDelegation = {
-  delegator_address: "",
-  validator_address: "",
-  shares: "0",
-  balance: { amount: "0", denom: "" },
-};
+interface ValidatorData {
+  readonly tokenInfo: TokenInfo;
+  readonly investment: Investment;
+  readonly balance: string;
+  readonly claims: Claims;
+}
 
-function getDelegationDataMap(delegation: StakingDelegation) {
-  if (!delegation) return;
+function getValidatorDataMap(validatorData: ValidatorData) {
+  if (!validatorData) return {};
 
-  const shares = formatShares(delegation.shares);
-  const balance = nativeCoinToDisplay(delegation.balance, config.coinMap).amount;
+  const stakePerToken = validatorData.investment.nominal_value;
+  const balance = Decimal.fromAtomics(validatorData.balance, validatorData.tokenInfo.decimals).toString();
+  const claims = validatorData.claims.claims.toString();
 
-  return { Shares: shares, Balance: balance };
+  return { "Stake/Token": stakePerToken, Balance: balance, Claims: claims };
 }
 
 interface WalletParams {
@@ -34,19 +34,25 @@ interface WalletParams {
 
 export function Wallet(): JSX.Element {
   const history = useHistory();
-  const { getStakingClient } = useSdk();
-  const { account } = useAccount();
   const { validatorAddress } = useParams<WalletParams>();
-  const validator = useStakingValidator(validatorAddress);
+  const { getClient } = useSdk();
+  const { account } = useAccount();
 
-  const [delegation, setDelegation] = useState<StakingDelegation>(emptyDelegation);
+  const [validatorData, setValidatorData] = useState<ValidatorData>();
 
   useEffect(() => {
-    getStakingClient()
-      .staking.delegation(account.address, validatorAddress)
-      .then(({ result: delegation }) => setDelegation(delegation))
-      .catch(console.error);
-  }, [getStakingClient, account.address, validatorAddress]);
+    const client = getClient();
+
+    client.getContract(validatorAddress).then(async (contract) => {
+      const cw20contract = CW20(client).use(contract.address);
+      const tokenInfo = await cw20contract.tokenInfo();
+      const investment = await cw20contract.investment();
+      const balance = await cw20contract.balance(account.address);
+      const claims = await cw20contract.claims(account.address);
+
+      setValidatorData({ tokenInfo, investment, balance, claims });
+    });
+  }, [getClient, validatorAddress, account.address]);
 
   function goToValidatorDetail() {
     history.push(`${pathValidator}/${validatorAddress}${pathDetail}`);
@@ -69,14 +75,14 @@ export function Wallet(): JSX.Element {
       <MainStack>
         <HeaderBackMenu />
         <TitleNavStack>
-          <Title>{validator?.description.moniker ?? ""}</Title>
+          <Title>{validatorData?.tokenInfo.name ?? ""}</Title>
           <NavCenter>
             <Button type="primary" onClick={goToValidatorDetail}>
               View Details
             </Button>
           </NavCenter>
         </TitleNavStack>
-        <DataList {...getDelegationDataMap(delegation)} />
+        <DataList {...getValidatorDataMap(validatorData)} />
         <ButtonStack>
           <Button type="primary" onClick={goToPurchase}>
             Buy
@@ -84,7 +90,8 @@ export function Wallet(): JSX.Element {
           <Button type="primary" onClick={goToWithdraw}>
             Withdraw
           </Button>
-          <Button type="primary" onClick={goToClaims}>
+          {/* Disable while claims are only a number */}
+          <Button disabled type="primary" onClick={goToClaims}>
             Claims
           </Button>
         </ButtonStack>
