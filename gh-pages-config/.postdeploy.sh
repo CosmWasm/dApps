1a221328f99479326fc3cda409fba02f85b7cee0
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-14 14:28:42 +0200

    Add staking-service to deployment

--- gh-pages-config/.postdeploy.sh
+++ gh-pages-config/.postdeploy.sh
@@ -4,3 +4,4 @@
 rm -r gh-pages-config/names
 rm -r gh-pages-config/wallet
 rm -r gh-pages-config/cw20-wallet
+rm -r gh-pages-config/stakes
--- gh-pages-config/.predeploy.sh
+++ gh-pages-config/.predeploy.sh
@@ -7,3 +7,4 @@ yarn build
 cp -R packages/name-service/build gh-pages-config/names
 cp -R packages/wallet/build gh-pages-config/wallet
 cp -R packages/cw20-wallet/build gh-pages-config/cw20-wallet
+cp -R packages/staking-service/build gh-pages-config/stakes
