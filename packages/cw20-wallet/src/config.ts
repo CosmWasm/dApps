0f37da31c72888d2860749487a570f9fdaf41574
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-21 11:03:59 +0200

    Adapt dApps config

--- packages/cw20-wallet/src/config.ts
+++ packages/cw20-wallet/src/config.ts
@@ -1,27 +1,79 @@
-import { AppConfig, CoinMap } from "@cosmicdapp/logic";
+import { AppConfig } from "@cosmicdapp/logic";
 
 const local: AppConfig = {
-  httpUrl: "http://localhost:1317",
-  feeToken: "ucosm",
-  gasPrice: 0.025,
-  faucetUrl: "http://localhost:8000/credit",
-  faucetToken: "COSM",
+  chainId: "cosmwasm-cosmos",
   addressPrefix: "cosmos",
-};
-
-const coinMap: CoinMap = {
-  ushell: { denom: "SHELL", fractionalDigits: 6 },
-  ureef: { denom: "REEF", fractionalDigits: 6 },
+  httpUrl: "http://localhost:1317",
+  faucetUrl: "http://localhost:8000/credit",
+  feeToken: "ucosm",
+  stakingToken: "uatom",
+  faucetToken: "COSM",
+  coinMap: {
+    ucosm: { denom: "COSM", fractionalDigits: 6 },
+    uatom: { denom: "ATOM", fractionalDigits: 6 },
+  },
+  gasPrice: 0.025,
+  codeId: 40,
 };
 
 const coralnet: AppConfig = {
-  httpUrl: "https://lcd.coralnet.cosmwasm.com",
-  feeToken: "ushell",
-  gasPrice: 0.025,
-  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
-  faucetToken: "SHELL",
+  chainId: "cosmwasm-coral",
   addressPrefix: "coral",
-  coinMap: coinMap,
+  httpUrl: "https://lcd.coralnet.cosmwasm.com",
+  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
+  feeToken: "ushell",
+  stakingToken: "ureef",
+  faucetToken: "SHELL",
+  coinMap: {
+    ushell: { denom: "SHELL", fractionalDigits: 6 },
+    ureef: { denom: "REEF", fractionalDigits: 6 },
+  },
+  gasPrice: 0.025,
+  keplrConfig: {
+    chainId: "cosmwasm-coral",
+    chainName: "Coral",
+    rpc: "https://rpc.coralnet.cosmwasm.com/",
+    rest: "https://lcd.coralnet.cosmwasm.com/",
+    bech32Config: {
+      bech32PrefixAccAddr: `coral`,
+      bech32PrefixAccPub: `coralpub`,
+      bech32PrefixValAddr: `coralvaloper`,
+      bech32PrefixValPub: `coralvaloperpub`,
+      bech32PrefixConsAddr: `coralvalcons`,
+      bech32PrefixConsPub: `coralvalconspub`,
+    },
+    currencies: [
+      {
+        coinDenom: "SHELL",
+        coinMinimalDenom: "ushell",
+        coinDecimals: 6,
+      },
+      {
+        coinDenom: "REEF",
+        coinMinimalDenom: "ureef",
+        coinDecimals: 6,
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "SHELL",
+        coinMinimalDenom: "ushell",
+        coinDecimals: 6,
+      },
+    ],
+    stakeCurrency: {
+      coinDenom: "REEF",
+      coinMinimalDenom: "ureef",
+      coinDecimals: 6,
+    },
+    gasPriceStep: {
+      low: 0.0125,
+      average: 0.025,
+      high: 0.05,
+    },
+    bip44: { coinType: 118 },
+    coinType: 118,
+  },
   codeId: 40,
 };
 
--- packages/staking-service/src/config.ts
+++ packages/staking-service/src/config.ts
@@ -1,29 +1,80 @@
-import { AppConfig, CoinMap } from "@cosmicdapp/logic";
+import { AppConfig } from "@cosmicdapp/logic";
 
 const local: AppConfig = {
+  chainId: "cosmwasm-cosmos",
   addressPrefix: "cosmos",
-  gasPrice: 0.025,
   httpUrl: "http://localhost:1317",
-  feeToken: "ucosm",
   faucetUrl: "http://localhost:8000/credit",
+  feeToken: "ucosm",
+  stakingToken: "uatom",
   faucetToken: "COSM",
-};
-
-const coinMap: CoinMap = {
-  ushell: { denom: "SHELL", fractionalDigits: 6 },
-  ureef: { denom: "REEF", fractionalDigits: 6 },
+  coinMap: {
+    ucosm: { denom: "COSM", fractionalDigits: 6 },
+    uatom: { denom: "ATOM", fractionalDigits: 6 },
+  },
+  gasPrice: 0.025,
+  codeId: 107,
 };
 
 const coralnet: AppConfig = {
+  chainId: "cosmwasm-coral",
   addressPrefix: "coral",
-  gasPrice: 0.025,
   httpUrl: "https://lcd.coralnet.cosmwasm.com",
+  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
   feeToken: "ushell",
   stakingToken: "ureef",
-  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
   faucetToken: "SHELL",
+  coinMap: {
+    ushell: { denom: "SHELL", fractionalDigits: 6 },
+    ureef: { denom: "REEF", fractionalDigits: 6 },
+  },
+  gasPrice: 0.025,
+  keplrConfig: {
+    chainId: "cosmwasm-coral",
+    chainName: "Coral",
+    rpc: "https://rpc.coralnet.cosmwasm.com/",
+    rest: "https://lcd.coralnet.cosmwasm.com/",
+    bech32Config: {
+      bech32PrefixAccAddr: `coral`,
+      bech32PrefixAccPub: `coralpub`,
+      bech32PrefixValAddr: `coralvaloper`,
+      bech32PrefixValPub: `coralvaloperpub`,
+      bech32PrefixConsAddr: `coralvalcons`,
+      bech32PrefixConsPub: `coralvalconspub`,
+    },
+    currencies: [
+      {
+        coinDenom: "SHELL",
+        coinMinimalDenom: "ushell",
+        coinDecimals: 6,
+      },
+      {
+        coinDenom: "REEF",
+        coinMinimalDenom: "ureef",
+        coinDecimals: 6,
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "SHELL",
+        coinMinimalDenom: "ushell",
+        coinDecimals: 6,
+      },
+    ],
+    stakeCurrency: {
+      coinDenom: "REEF",
+      coinMinimalDenom: "ureef",
+      coinDecimals: 6,
+    },
+    gasPriceStep: {
+      low: 0.0125,
+      average: 0.025,
+      high: 0.05,
+    },
+    bip44: { coinType: 118 },
+    coinType: 118,
+  },
   codeId: 107,
-  coinMap,
 };
 
 // REACT_APP_LOCAL is set via `yarn start:local`
