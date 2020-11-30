6dad83c10fd348b35e429ceb772f2a1797031f1c
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-11-30 16:59:35 +0100

    Update template config

--- packages/cra-template/template/src/config.ts
+++ packages/cra-template/template/src/config.ts
@@ -1,8 +1,4 @@
-import { AppConfig } from "@cosmicdapp/logic";
-
-interface NetworkConfigs {
-  readonly [key: string]: AppConfig;
-}
+import { AppConfig, getAppConfig, NetworkConfigs } from "@cosmicdapp/logic";
 
 const local: AppConfig = {
   chainId: "testing",
@@ -13,27 +9,9 @@ const local: AppConfig = {
   faucetUrl: "http://localhost:8000/credit",
   feeToken: "ucosm",
   stakingToken: "uatom",
-  faucetToken: "COSM",
   coinMap: {
     ucosm: { denom: "COSM", fractionalDigits: 6 },
     uatom: { denom: "ATOM", fractionalDigits: 6 },
-  },
-  gasPrice: 0.025,
-};
-
-const coralnet: AppConfig = {
-  chainId: "cosmwasm-coral",
-  chainName: "Coral",
-  addressPrefix: "coral",
-  rpcUrl: "https://rpc.coralnet.cosmwasm.com",
-  httpUrl: "https://lcd.coralnet.cosmwasm.com",
-  faucetUrl: "https://faucet.coralnet.cosmwasm.com/credit",
-  feeToken: "ushell",
-  stakingToken: "ureef",
-  faucetToken: "SHELL",
-  coinMap: {
-    ushell: { denom: "SHELL", fractionalDigits: 6 },
-    ureef: { denom: "REEF", fractionalDigits: 6 },
   },
   gasPrice: 0.025,
 };
@@ -42,12 +20,11 @@ const heldernet: AppConfig = {
   chainId: "hackatom-wasm",
   chainName: "Helder",
   addressPrefix: "cosmos",
-  rpcUrl: "https://rpc.heldernet.cosmwasm.com/",
-  httpUrl: "https://lcd.heldernet.cosmwasm.com/",
+  rpcUrl: "https://rpc.heldernet.cosmwasm.com",
+  httpUrl: "https://lcd.heldernet.cosmwasm.com",
   faucetUrl: "https://faucet.heldernet.cosmwasm.com/credit",
   feeToken: "ucosm",
   stakingToken: "ustake",
-  faucetToken: "COSM",
   coinMap: {
     ucosm: { denom: "COSM", fractionalDigits: 6 },
     ustake: { denom: "STAKE", fractionalDigits: 6 },
@@ -55,18 +32,5 @@ const heldernet: AppConfig = {
   gasPrice: 0.025,
 };
 
-const configs: NetworkConfigs = { local, coralnet, heldernet };
-
-function getAppConfig(): AppConfig {
-  const network = process.env.REACT_APP_NETWORK;
-  if (!network) return local;
-
-  const config = configs[network];
-  if (!config) {
-    throw new Error(`No configuration found for network ${network}`);
-  }
-
-  return config;
-}
-
-export const config = getAppConfig();
+const configs: NetworkConfigs = { local, heldernet };
+export const config = getAppConfig(configs);
