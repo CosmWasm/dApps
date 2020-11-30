69f125614c397d49c6ae9d7f4014419876ff8628
Simon Warta <simon@warta.it>
2020-11-30 19:30:04 +0100

    Improve .d.ts script

--- packages/cra-template/template/src/contracts/scripts/get_dts.sh
+++ packages/cra-template/template/src/contracts/scripts/get_dts.sh
@@ -5,10 +5,11 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 NORMALIZER="$(yarn bin)/json-schema-ref-parser-cli"
 SCHEMA2DTS="$(yarn bin)/json-schema-to-typescript-cli"
 
-SOURCE_DIR="$1"
-# Leave .d.ts file in "types/<contract-name>" directory, sibling of $SOURCE_DIR
-TYPES_DIR=$(echo "$SOURCE_DIR" | cut -f 1 -d '/')/types
+# Leave .d.ts file in "./types/<contract-name>" directory, relative to the current dir
+TYPES_DIR=./types
 TYPE_FILE=$(basename "$SOURCE_DIR").d.ts
+mkdir -p "$TYPES_DIR"
+OUT_FILE="$TYPES_DIR/$TYPE_FILE"
 
 {
   echo "/*"
@@ -19,15 +20,15 @@ TYPE_FILE=$(basename "$SOURCE_DIR").d.ts
   echo " * Type generation powered by json-schema-to-typescript."
   echo " */"
   echo ""
-}
+} > "$OUT_FILE"
 
 for SCHEMA in "$SOURCE_DIR"/schema/*.json; do
   NORMALIZED_SCHEMA=$(basename "$SCHEMA")
   echo "Processing $SCHEMA ..." >&2
   "$NORMALIZER" "$SCHEMA" > "$NORMALIZED_SCHEMA"
   (
-    "$SCHEMA2DTS" "$NORMALIZED_SCHEMA" >> "$TYPES_DIR"/"$TYPE_FILE"
-    echo "" >> "$TYPES_DIR"/"$TYPE_FILE"
+    "$SCHEMA2DTS" "$NORMALIZED_SCHEMA" >> "$OUT_FILE"
+    echo "" >> "$OUT_FILE"
     rm "$NORMALIZED_SCHEMA"
   )
 done
