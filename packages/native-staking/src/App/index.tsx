79b75b9232bd19d4af3eaf60ce4a26836c6fe69d
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-01-25 10:35:08 +0100

    Add index and config files

---
+++ packages/native-staking/src/App/index.tsx
@@ -0,0 +1,54 @@
+import { GlobalStyle } from "@cosmicdapp/design";
+import { ContractsProvider, ErrorProvider, ProtectedSwitch, SdkProvider } from "@cosmicdapp/logic";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { config } from "../config";
+import {
+  pathDetail,
+  pathLogin,
+  pathOperationResult,
+  pathPurchase,
+  pathValidator,
+  pathValidators,
+  pathWallet,
+  pathWithdraw,
+} from "./paths";
+import { Login } from "./routes/Login";
+import { OperationResult } from "./routes/OperationResult";
+import { Purchase } from "./routes/Purchase";
+import { ValidatorDetail } from "./routes/ValidatorDetail";
+import { ValidatorHome } from "./routes/ValidatorHome";
+import { Validators } from "./routes/Validators";
+import { Wallet } from "./routes/Wallet";
+import { Withdraw } from "./routes/Withdraw";
+
+export function App(): JSX.Element {
+  return (
+    <ErrorProvider>
+      <SdkProvider config={config}>
+        <ContractsProvider>
+          <GlobalStyle />
+          <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path={pathLogin} component={Login} />
+              <ProtectedSwitch authPath={pathLogin}>
+                <Route exact path={pathValidators} component={Validators} />
+                <Route exact path={`${pathValidator}/:validatorAddress`} component={ValidatorHome} />
+                <Route
+                  exact
+                  path={`${pathValidator}/:validatorAddress${pathDetail}`}
+                  component={ValidatorDetail}
+                />
+                <Route exact path={`${pathWallet}/:validatorAddress`} component={Wallet} />
+                <Route exact path={`${pathPurchase}/:validatorAddress`} component={Purchase} />
+                <Route exact path={`${pathWithdraw}/:validatorAddress`} component={Withdraw} />
+                <Route exact path={pathOperationResult} component={OperationResult} />
+              </ProtectedSwitch>
+            </Switch>
+          </Router>
+        </ContractsProvider>
+      </SdkProvider>
+    </ErrorProvider>
+  );
+}
---
+++ packages/native-staking/src/App/paths.ts
@@ -0,0 +1,8 @@
+export const pathLogin = "/login";
+export const pathValidators = "/validators";
+export const pathValidator = "/validator";
+export const pathDetail = "/detail";
+export const pathWallet = "/wallet";
+export const pathPurchase = "/purchase";
+export const pathWithdraw = "/withdraw";
+export const pathOperationResult = "/result";
