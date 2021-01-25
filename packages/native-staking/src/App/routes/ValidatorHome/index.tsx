0e7d66ec7cdd2016fc7ff56afadb782eb94002c9
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-01-25 10:36:22 +0100

    Add ValidatorHome route

---
+++ packages/native-staking/src/App/routes/ValidatorHome/index.tsx
@@ -0,0 +1,91 @@
+import { PageLayout } from "@cosmicdapp/design";
+import { nativeCoinToDisplay } from "@cosmicdapp/logic";
+import { Coin } from "@cosmjs/launchpad";
+import { Button, Typography } from "antd";
+import React from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { config } from "../../../config";
+import { DataList } from "../../components/DataList";
+import { HeaderBackMenu } from "../../components/HeaderBackMenu";
+import {
+  pathDetail,
+  pathPurchase,
+  pathValidator,
+  pathValidators,
+  pathWallet,
+  pathWithdraw,
+} from "../../paths";
+import { StakingValidator, useStakingValidator } from "../../utils/staking";
+import { ButtonStack, MainStack, NavCenter, TitleNavStack } from "./style";
+
+const { Title } = Typography;
+
+function getValidatorMap(validator?: StakingValidator) {
+  if (!validator) return {};
+
+  const tokensCoin: Coin = { denom: config.stakingToken, amount: validator.tokens };
+  const tokensAmount = nativeCoinToDisplay(tokensCoin, config.coinMap).amount;
+
+  return {
+    Tokens: tokensAmount,
+    "Delegator Shares": validator.delegator_shares,
+    "Min. Delegation": validator.min_self_delegation,
+  };
+}
+
+interface ValidatorHomeParams {
+  readonly validatorAddress: string;
+}
+
+export function ValidatorHome(): JSX.Element {
+  const history = useHistory();
+  const { validatorAddress } = useParams<ValidatorHomeParams>();
+
+  const validator = useStakingValidator(validatorAddress);
+
+  function goToWallet() {
+    history.push(`${pathWallet}/${validatorAddress}`);
+  }
+
+  function goToValidatorDetail() {
+    history.push(`${pathValidator}/${validatorAddress}${pathDetail}`);
+  }
+
+  function goToPurchase() {
+    history.push(`${pathPurchase}/${validatorAddress}`);
+  }
+
+  function goToWithdraw() {
+    history.push(`${pathWithdraw}/${validatorAddress}`);
+  }
+
+  return (
+    <PageLayout>
+      <MainStack>
+        <HeaderBackMenu path={pathValidators} />
+        <TitleNavStack>
+          <Title>{validator?.description.moniker ?? ""}</Title>
+          <NavCenter>
+            <ButtonStack>
+              <Button type="primary" onClick={goToWallet}>
+                Wallet
+              </Button>
+              <Button type="primary" onClick={goToValidatorDetail}>
+                View Details
+              </Button>
+            </ButtonStack>
+          </NavCenter>
+        </TitleNavStack>
+        <DataList {...getValidatorMap(validator)} />
+        <ButtonStack>
+          <Button type="primary" onClick={goToPurchase}>
+            Buy
+          </Button>
+          <Button type="primary" onClick={goToWithdraw}>
+            Withdraw
+          </Button>
+        </ButtonStack>
+      </MainStack>
+    </PageLayout>
+  );
+}
