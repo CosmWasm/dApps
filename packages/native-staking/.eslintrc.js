d3af015506961ac45bdc5a2bce98cf93b80fb1c0
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-01-25 10:33:47 +0100

    Add skeleton files for native-staking

---
+++ packages/native-staking/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended", // Make sure this is always the last configuration in the extends array
+  ],
+  rules: {
+    "default-case": "error",
+  },
+  overrides: [
+    {
+      files: "**/*.js",
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+};
---
+++ packages/native-staking/README.md
@@ -0,0 +1,18 @@
+# native-staking
+
+## Prerequisites
+
+In the root directory, run:
+
+```shell
+yarn
+yarn build-deps
+```
+
+## Run the app
+
+In the packages/native-staking directory, run:
+
+```shell
+yarn start
+```
