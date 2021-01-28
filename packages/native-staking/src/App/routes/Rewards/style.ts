d23997368fb2d477d62941d8eb09026956a983ae
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-01-28 19:38:56 +0100

    Add Rewards route

---
+++ packages/native-staking/src/App/routes/Rewards/style.ts
@@ -0,0 +1,38 @@
+import { Stack } from "@cosmicdapp/design";
+import styled from "styled-components";
+
+export const MainStack = styled(Stack)`
+  & > * {
+    --gap: var(--s6);
+  }
+
+  width: 100%;
+`;
+
+export const HeaderTitleStack = styled(Stack)`
+  & > * {
+    --gap: var(--s0);
+  }
+
+  h1.ant-typography,
+  h2.ant-typography {
+    margin: 0;
+    margin-top: var(--gap);
+  }
+
+  h1 {
+    font-size: var(--s2);
+    line-height: var(--s2);
+  }
+
+  h2 {
+    font-size: var(--s5);
+    line-height: var(--s5);
+  }
+`;
+
+export const ButtonStack = styled(Stack)`
+  & > * {
+    --gap: var(--s2);
+  }
+`;
