410e557ef01f68290b4a5df39631589760eca2a7
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-01-29 00:51:22 +0100

    Add deployment for native staking

--- gh-pages-config/.postdeploy.sh
+++ gh-pages-config/.postdeploy.sh
@@ -4,4 +4,5 @@
 rm -r gh-pages-config/names
 rm -r gh-pages-config/wallet
 rm -r gh-pages-config/cw20-wallet
-rm -r gh-pages-config/stakes
+rm -r gh-pages-config/staking
+rm -r gh-pages-config/cw20-staking
--- gh-pages-config/.predeploy.sh
+++ gh-pages-config/.predeploy.sh
@@ -7,4 +7,5 @@ yarn build
 cp -R packages/name-service/build gh-pages-config/names
 cp -R packages/wallet/build gh-pages-config/wallet
 cp -R packages/cw20-wallet/build gh-pages-config/cw20-wallet
-cp -R packages/staking-service/build gh-pages-config/stakes
+cp -R packages/native-staking/build gh-pages-config/staking
+cp -R packages/staking-service/build gh-pages-config/cw20-staking
