9229fa3670d4caeea1db44c683b5f4f1a41c28c8
peaceandwhisky <guitarfullife@gmail.com>
2021-03-21 00:05:48 +0900

    add config of hackatom-ru network and command

--- packages/cw20-wallet/src/config.ts
+++ packages/cw20-wallet/src/config.ts
@@ -34,5 +34,22 @@ const musselnet: AppConfig = {
   codeId: 4,
 };
 
-const configs: NetworkConfigs = { local, musselnet };
+const hackatom: AppConfig = {
+  chainId: "hackatom-ru",
+  chainName: "HackAtom RU",
+  addressPrefix: "wasm",
+  rpcUrl: "https://rpc.cosmwasm.hub.hackatom.org/",
+  httpUrl: "https://rest.cosmwasm.hub.hackatom.org/",
+  faucetUrl: "https://faucet.cosmwasm.hub.hackatom.org/",
+  feeToken: "ucosm",
+  stakingToken: "uatom",
+  coinMap: {
+    ucosm: { denom: "COSM", fractionalDigits: 6 },
+    uatom: { denom: "ATOM", fractionalDigits: 6 },
+  },
+  gasPrice: 0.025,
+  codeId: 4,
+};
+
+const configs: NetworkConfigs = { local, musselnet, hackatom };
 export const config = getAppConfig(configs);
